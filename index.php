a03812828c1248008155ce4570502910ae5ae2f0
JonissonGomes <42873530+JonissonGomes@users.noreply.github.com>
2018-10-02 15:26:25 -0300

    add novo index
    Atualizando

--- index.php
+++ index.php
@@ -1,18 +1,24 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
 	<meta charset="UTF-8">
 	<title>Início</title> 
-	<link rel="stylesheet" type="text/css" href="CSS/barraL.css" >
-	<link rel="shortcut icon" href="icon.png" >
+	<link rel="stylesheet" type="text/css" href="css/barraL.css" >
+	<link rel="shortcut icon" href="img/icon.png" >
 </head>
 <body>
-<div class="lc">
-		<a href="cadastro.php"> <button class="sendtop" > Cadastrar-se</button></a>
-		<a href="login.php"> <button class="sendtop"> Login</button></a>
+     <div class="lc">
+     <?php
+    if(!isset($_SESSION['user'])){ ?>
+		<a href="php/cadastro.php"> <button class="sendtop" > Cadastrar-se</button></a>
+		<a href="php/login.php"> <button class="sendtop"> Login</button></a>
+	<?php }else{ ?>
+	    <a href="php/logout.php"> <button class="sendtop"> Sair </button> </a>
+	    <?php } ?>
 	</div>
 	<div id="menu">
-		<img class="logo" src="CSS/logo.png">
+		<img class="logo" src="img/logo.png">
 		<ul>
 			<li><a href="#" class="active">Home</a></li>
 			<li><a href="#">Notícias</a></li>
