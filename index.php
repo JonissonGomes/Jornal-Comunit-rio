db2a1b51422447661ad0379f92f07bdb2a23e523
Silvio Paiva <42873611+majorsilvio@users.noreply.github.com>
2018-10-04 19:40:44 -0300

    att da pagina index.

--- index.php
+++ index.php
@@ -6,27 +6,8 @@
 	<title>Início</title> 
 	<link rel="stylesheet" type="text/css" href="css/barraL.css" >
 	<link rel="shortcut icon" href="img/icon.png" >
+ 	<?php include('php/bar.php'); ?>
 </head>
 <body>
-     <div class="lc">
-     <?php
-    if(!isset($_SESSION['user'])){ ?>
-		<a href="php/cadastro.php"> <button class="sendtop" > Cadastrar-se</button></a>
-		<a href="php/login.php"> <button class="sendtop"> Login</button></a>
-	<?php }else{ ?>
-	    <a href="php/logout.php"> <button class="sendtop"> Sair </button> </a>
-	    <?php } ?>
-	</div>
-	<div id="menu">
-		<img class="logo" src="img/logo.png">
-		<ul>
-			<li><a href="#" class="active">Home</a></li>
-			<li><a href="#">Notícias</a></li>
-			<li><a href="#">Comunidades</a></li>
-			<li><a href="#">Sobre</a></li>
-		</ul>
-	</div>
-
-
 </body>
 </html>
