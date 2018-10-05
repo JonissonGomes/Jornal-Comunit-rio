469a8335c7a1e0897ee265e9212df553591bd446
adrienesantana01 <42873660+adrienesantana01@users.noreply.github.com>
2018-10-05 12:38:25 -0300

    atualização do index;

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
 	<title>Início</title> 
 	<link rel="stylesheet" type="text/css" href="css/barraL.css" >
 	<link rel="shortcut icon" href="img/icon.png" >
- 	<?php include('php/bar.php'); ?>
 </head>
 <body>
+<?php include('html/index.html'); ?>
 </body>
 </html>
