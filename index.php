a5070e4ebed3e069e701f5f7ceccf1cfb30da216
JonissonGomes <42873530+JonissonGomes@users.noreply.github.com>
2018-11-14 15:14:49 -0300

    Atulização Index
    Correção de bugs e comentários

--- index.php
+++ index.php
@@ -11,7 +11,6 @@
 	<link rel="shortcut icon" href="img/icon.png" >
 	<?php include('php/bar.php') ?>
 </head>
-<body>
 <div class="container">
   <h2></h2>  
   <div id="myCarousel" class="carousel slide" data-ride="carousel">
@@ -22,37 +21,39 @@
       <li data-target="#myCarousel" data-slide-to="2"></li>
     </ol>
 
-    <!-- Wrapper for slides -->
+    <!-- Áreas do SlideShow -->
     <div class="carousel-inner">
       <div class="item active">
         <img src="/img/igarassu.jpg" alt="" style="width:100%;filter: brightness(50%)">
         <div class="item">
           <div class="carousel-caption">
-            <h3>Web Jornal</h3>
-            <p>As notícias sobre a sua comunidade você encontra nesta plataforma.</p>
+            <h3>BEM VINDO</h3>
+            <p>As notícias sobre a sua comunidade você encontra nesta plataforma online.</p>
         </div>
     </div>
+    <!-- Slide 2 -->
       </div>
       <div class="item">
         <img src="/img/igarassu2.jpg" alt="" style="width:100%;filter: brightness(50%)">
         <div class="item">
         <div class="carousel-caption">
           <h3>O que o Web Jornal oferece?</h3>
-          <p>Web Jornal oferece ao usuário a possibilidade de fazer publicações sobre problemas locais e a receber feedback sobre a resolução dos problemas. </p>
+          <p>Nós oferecemos a você usuário a possibilidade de fazer publicações sobre problemas locais e a receber feedback sobre a resolução dos problemas. </p>
           </div>
         </div>
       </div>
+     <!-- Slide 3 -->
       <div class="item">
         <img src="/img/a.jpg" alt="" style="width:100%;filter: brightness(50%)">
         <div class="item">
           <div class="carousel-caption">
-           <h3>Web Jornal</h3>
            <p>Faça publicações sobre os problemas da sua cidade, receba feedbacks e contribua para a melhoria da sua comunidade.</p>
           </div>
         </div>
       </div>
     </div>
-    <!-- Left and right controls -->
+
+    <!-- Controles de direção (Esquerda e Direita) -->
     <a class="left carousel-control" href="#myCarousel" data-slide="prev">
       <span class="glyphicon glyphicon-chevron-left"></span>
       <span class="sr-only">Previous</span>
@@ -63,34 +64,46 @@
     </a>
   </div>
 </div>
-	<div class="alinha">
-	<h3>Sobre o que você pode publicar?</h3><br>
-	<br>
-	<fieldset class="foto1">
-		<img  class="aline" src="../img/saude.png" width="150" height="150">
-		<p> Saúde	
-	</fieldset>
 
-	<fieldset class="foto1">
-		<img src="../img/agua.png" width="150" height="150">
-		<p>Saneamento básico
-	</fieldset>
 
-	<fieldset class="foto1">
-		<img src="../img/educacao.png" width="150" height="150">
-		<p> Educação
-	</fieldset>
+<!-- Área de seleção abaixo do SlideShow -->
+  <div class="alinha">
+  <h3>Sobre o que você pode publicar?</h3><br>
+  <br>
+  <fieldset class="foto1">
+  <button type="button" class="btn btn-info" id="categorias">
+    <img  class="aline" src="../img/saude.png" width="150" height="150">
+    <p> Saúde
+    </button>  
+  </fieldset>
 
-	<fieldset class="foto1">
-		<img src="../img/saude.png" width="150" height="150">
+  <fieldset class="foto1" id="border-box">
+  <button type="button" class="btn btn-info" id="categorias">
+    <img src="../img/agua.png" width="150" height="150">
+    <p>Saneamento básico
+    </button>
+  </fieldset>
 
-	</fieldset>
+  <fieldset class="foto1">
+  <button type="button" class="btn btn-info" id="categorias">
+    <img src="../img/educacao.png" width="150" height="150">
+    <p> Educação
+    </button>
+  </fieldset>
 
-	<fieldset class="foto1">
-		<img src="../img/saude.png" width="150" height="150">
+  <fieldset class="foto1">
+  <button type="button" class="btn btn-info" id="categorias">
+    <img src="../img/social.png" width="150" height="150">
+    <p> Ação Social
+    </button>
+  </fieldset>
 
-	</fieldset>
+  <fieldset class="foto1">
+  <button type="button" class="btn btn-info" id="categorias">
+    <img src="../img/ambiente.png" width="150" height="150">
+    <p> Ambiente
+    </button>
+  </fieldset>
 
-	</div>
-</body>
+  </div>
 </html>
